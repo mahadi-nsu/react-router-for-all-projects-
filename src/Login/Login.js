47efd19bcb0ee0eb71a8f7d1de647ac5fef18e57
mahadi-nsu <mahadi.hassan@northsouth.edu>
2021-08-29 00:42:20 +0600

    some more components added

---
+++ src/Login/Login.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+  return <div>I am Login and i dont want a navbar!</div>;
+};
+
+export default Login;
---
+++ src/Register/Register.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Register = () => {
+  return <div>I am Register and i dont want a navbar!</div>;
+};
+
+export default Register;
