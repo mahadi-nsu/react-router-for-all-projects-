2cf142ba593b42055f848e7c75fe7af681922e77
mahadi-nsu <mahadi.hassan@northsouth.edu>
2021-08-28 21:55:32 +0600

    navbar added to switch between components

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 import Admin from "./Admin/Admin";
 import Products from "./Products/Products";
 import { css } from "@emotion/css";
+import Nav from "./Common/Nav";
 
 const AppStyles = css`
   margin: 50px auto;
@@ -24,6 +25,7 @@ const App = () => {
     <div className={AppStyles}>
       <Router>
         <div className="Container">
+          <Nav />
           <Routes>
             <Route path="/" element={<Products />} />
             <Route path="/admin" element={<Admin />} />
