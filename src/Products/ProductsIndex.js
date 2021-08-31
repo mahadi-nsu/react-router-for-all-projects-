f6467599b8acd3df467981fecdf21cc17289c000
mahadi-nsu <mahadi.hassan@northsouth.edu>
2021-08-31 09:58:14 +0600

    navigate handled when params doesn't match

--- src/Products/ProductsIndex.js
+++ src/Products/ProductsIndex.js
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { listProducts } from "./ProductsService";
 import ProductCard from "./ProductCard";
 
 const ProductsIndex = () => {
+  const { state } = useLocation();
+  console.log(location);
   const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    if (state) {
+      console.warn(`Nothing found for ${state.id}`);
+    }
+  });
 
   useEffect(() => {
     (async () => {
